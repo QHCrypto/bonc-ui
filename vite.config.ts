0a7d2e14aaa7e7873323fe6b103228f309dbf1c9
Guyutongxue <guyutongxue@163.com>
2026-01-19 09:22:40 +0800

    hosting

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { nitro } from 'nitro/vite'
 const config = defineConfig({
   plugins: [
     devtools(),
-    // nitro(),
+    nitro({ preset: "bun" }),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
